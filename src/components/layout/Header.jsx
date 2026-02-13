64ee1947b55a1e93991285df959d987033c73faf
omadzelevan <levanomadze13@gmail.com>
2026-02-13 13:30:57 +0400

    changed style struct

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,29 +1,22 @@
 import "../../styles/globals.css";
 import burger from "../../assets/icons/hamburger 1.svg";
+import Logo from "/home/lea/tours/tours-ge/src/assets/images/logo.png";
 
 export default function Header() {
   return (
     <header className="header">
-      <div className="container header-inner">
-        <div className="logo">
-          <h1>Van Travel Georgia</h1>
-        </div>
-        <nav className="nav">
-          <a href="#tours">Tours</a>
-          <a href="#why-toru">Why Toru</a>
-          <a href="#itineraries">Itineraries</a>
-          <a href="#stories">Stories</a>
-          <a href="#faq">FAQ</a>
-        </nav>
-        <div className="header-cta">
-          <button className="btn btn-ghost">Call +995 555 123 456</button>
-          <button className="btn btn-primary">Plan My Trip</button>
+      <div className="logo">
+        <img src={Logo} alt="" />
+      </div>
+      <div className="nav">
+        <div className="lang-chose">
+          <select>
+            <option value="en">🇬🇧</option>
+            <option value="ge">🇬🇪</option>
+          </select>
         </div>
         <div className="burger">
-          <button className="burger-btn" aria-label="Open menu">
-            <img className="burger-img" src={burger} alt="" />
-            Menu
-          </button>
+          <img src={burger} alt="" />
         </div>
       </div>
     </header>
